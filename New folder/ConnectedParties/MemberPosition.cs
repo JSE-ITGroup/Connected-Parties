2f2316a3e798c6d96d183c79d97e684e42d4129e
Michael Edwards <michael.edwards@jamstockex.com>
2018-05-28 11:24:20 -0500

    Commit initial existing files

---
+++ New folder/ConnectedParties/MemberPosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConnectedParties
+{
+    public class MemberPosition
+    {
+        public int KeyMemberAccountID { get; set; }
+        public string KeyMemberTRN { get; set; }
+        public string KeyMemberName { get; set; }
+        public string KeyMemberIssuer { get; set; }
+        public string KeyMemberPosition { get; set; }
+        public DateTime KeyMemberJoinDate { get; set; }
+        public DateTime KeyMemberResignDate { get; set; }
+        public string EnteredBy { get; set; }
+        public DateTime EnteredAt { get; set; }
+    }
+}
